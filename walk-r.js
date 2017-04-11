426614cf8cd7c23beb9b1751581231ad1e13a1bc
Nathaniel Martin <nate.teh.greatt@gmail.com>
2017-04-10 21:25:58 -0400

    made path platform agnostic

--- walk-r.js
+++ walk-r.js
@@ -1,4 +1,5 @@
 let fs = require('fs')
+let path = require('path')
 let R = require('ramda')
 
 let readDir = dir => fs.readdirSync(dir)
@@ -9,15 +10,15 @@ let isFile = R.pipe(isFolder, R.not) // isFolder? jk it's not
 let getFolders = R.filter(isFolder)
 let getFiles = R.filter(isFile)
 
-let prependPath = path => R.map(f => path+'/'+f)
+let prependPath = acc => R.map(f => path.join(acc,f))
 
-let walk = (dir, path) => {
-  path = path ? path+'/'+dir : dir
-  let nodes = readDir(path)
+let walk = (dir, acc) => {
+  acc = acc ? path.join(acc,dir) : dir
+  let nodes = readDir(acc)
   let folders = getFolders(nodes)
-  let files = R.pipe(getFiles, prependPath(path))(nodes)
+  let files = R.pipe(getFiles, prependPath(acc))(nodes)
   return R.pipe(
-    R.map(f => walk(f,path)),
+    R.map(f => walk(f,acc)),
     R.append(files),
     R.flatten
   )(folders)
